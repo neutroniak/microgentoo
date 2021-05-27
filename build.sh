4509d55bf49b22de638364b271d2eba5599d2ed7
Marcos Rodrigues Gonzalez <mrgonzalez1275@gmail.com>
2021-05-27 13:02:52 -0300

    zeromq: initial

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ buildah bud -f gentoo-container-base -t gentoo-container-base:latest
 buildah bud -f gentoo-container-gcc -t gentoo-container-gcc:latest
 buildah bud -f gentoo-container-python3 -t gentoo-container-python3:latest
 buildah bud -f gentoo-container-nodejs -t gentoo-container-nodejs:latest
+buildah bud -f gentoo-container-zeromq -t gentoo-container-zeromq:latest
 
 mv test /usr/lib/python3.8/
 mv docs /usr/lib64/node_modules/npm/
@@ -15,4 +16,5 @@ buildah push gentoo-container-base:latest ${REGISTRY_URL}/gentoo-container-base:
 buildah push gentoo-container-gcc:latest ${REGISTRY_URL}/gentoo-container-gcc:latest
 buildah push gentoo-container-python3:latest ${REGISTRY_URL}/gentoo-container-python3:latest
 buildah push gentoo-container-nodejs:latest ${REGISTRY_URL}/gentoo-container-nodejs:latest
+buildah push gentoo-container-zeromq:latest ${REGISTRY_URL}/gentoo-container-zeromq:latest
 
