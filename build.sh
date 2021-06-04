ec28ff345e9dadcbbcff4b5845f3d5cadaaf8536
Marcos Rodrigues Gonzalez <mrgonzalez1275@gmail.com>
2021-06-04 10:18:14 -0300

    general: gcc and python versions from env

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-mv /usr/lib/python3.8/test .
-mv /usr/lib/python3.8/site-packages .
-mkdir /usr/lib/python3.8/site-packages
-cp site-packages/epython.py /usr/lib/python3.8/site-packages/
+export GCC_VERSION=$(gcc --version |rev |grep ccg|awk '{print $1}'|rev)
+export PYTHON_VERSION="3.9"
+
+mv /usr/lib/python${PYTHON_VERSION}/test .
+mv /usr/lib/python${PYTHON_VERSION}/site-packages .
+mkdir /usr/lib/python${PYTHON_VERSION}/site-packages
+cp site-packages/epython.py /usr/lib/python${PYTHON_VERSION}/site-packages/
 
 mv /usr/lib64/node_modules/npm/docs .
 
-buildah bud -f gentoo-container-base -t gentoo-container-base:latest
-buildah bud -f gentoo-container-gcc -t gentoo-container-gcc:latest
-buildah bud -f gentoo-container-python3 -t gentoo-container-python3:latest
+buildah bud --build-arg GCC_VERSION=$GCC_VERSION -f gentoo-container-base -t gentoo-container-base:latest
+buildah bud --build-arg GCC_VERSION=$GCC_VERSION -f gentoo-container-gcc -t gentoo-container-gcc:latest
+buildah bud --build-arg PYTHON_VERSION=$PYTHON_VERSION -f gentoo-container-python3 -t gentoo-container-python3:latest
 buildah bud -f gentoo-container-nodejs -t gentoo-container-nodejs:latest
 buildah bud -f gentoo-container-zeromq -t gentoo-container-zeromq:latest
+buildah bud -f gentoo-container-nginx -t gentoo-container-nginx:latest
 
-mv test /usr/lib/python3.8/
-rm -rf /usr/lib/python3.8/site-packages
-mv site-packages /usr/lib/python3.8/
+mv test /usr/lib/python${PYTHON_VERSION}/
+rm -rf /usr/lib/python${PYTHON_VERSION}/site-packages
+mv site-packages /usr/lib/python${PYTHON_VERSION}/
 mv docs /usr/lib64/node_modules/npm/
 
 buildah push gentoo-container-base:latest ${REGISTRY_URL}/gentoo-container-base:latest
