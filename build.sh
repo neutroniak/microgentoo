36cb706815adf99180d4a261ea34831df29f3282
Marcos Rodrigues Gonzalez <mrgonzalez1275@gmail.com>
2021-06-18 11:24:46 -0300

    registry_args for buildah pushes

--- build.sh
+++ build.sh
@@ -22,9 +22,9 @@ rm -rf /usr/lib/python${PYTHON_VERSION}/site-packages
 mv site-packages /usr/lib/python${PYTHON_VERSION}/
 mv docs /usr/lib64/node_modules/npm/
 
-buildah push gentoo-container-base:latest ${REGISTRY_URL}/gentoo-container-base:latest
-buildah push gentoo-container-gcc:latest ${REGISTRY_URL}/gentoo-container-gcc:latest
-buildah push gentoo-container-python3:latest ${REGISTRY_URL}/gentoo-container-python3:latest
-buildah push gentoo-container-nodejs:latest ${REGISTRY_URL}/gentoo-container-nodejs:latest
-buildah push gentoo-container-zeromq:latest ${REGISTRY_URL}/gentoo-container-zeromq:latest
+buildah push ${REGISTRY_ARGS} gentoo-container-base:latest ${REGISTRY_URL}/gentoo-container-base:latest
+buildah push ${REGISTRY_ARGS} gentoo-container-gcc:latest ${REGISTRY_URL}/gentoo-container-gcc:latest
+buildah push ${REGISTRY_ARGS} gentoo-container-python3:latest ${REGISTRY_URL}/gentoo-container-python3:latest
+buildah push ${REGISTRY_ARGS} gentoo-container-nodejs:latest ${REGISTRY_URL}/gentoo-container-nodejs:latest
+buildah push ${REGISTRY_ARGS} gentoo-container-zeromq:latest ${REGISTRY_URL}/gentoo-container-zeromq:latest
 
